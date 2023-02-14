4226db787b602a8b895e2ad1e2d2be4ff1b91e68
xjarnot <30859263+xjarnot@users.noreply.github.com>
2023-02-14 13:48:50 +0100

    Initial commit

---
+++ test/sqlsrdevapiv.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Sqlsrdevapiv from '../lib/sqlsrdevapiv-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sqlsrdevapiv-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Sqlsrdevapiv.SqlsrdevapivStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
